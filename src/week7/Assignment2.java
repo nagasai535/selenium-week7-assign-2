1973c901842b0008b29a679f8ba3531ef60fa084
satish.wingherry <satish.wingherry@gmail.com>
2021-08-28 14:49:39 +0530

    Naga sai assignment

---
+++ src/week7/Assignment2.java
@@ -0,0 +1,23 @@
+package week7;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\tools\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		System.out.println("Welcome  to Ebay...");
+		driver.get("https://www.ebay.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		driver.close();
+		
+		
+		
+
+	}
+
+}
